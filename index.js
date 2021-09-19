790210458491b91768886e58e4bfa4a7d02b692b
farheenfatima3 <farheenfatima961@gmail.com>
2021-09-19 22:51:32 +0530

    added progress bar

--- index.js
+++ index.js
@@ -3,23 +3,29 @@ const playSound = document.querySelector(".audio")
 const image = document.querySelector(".image")
 const title = document.querySelector(".title")
 const artist = document.querySelector(".artist")
-const next=document.getElementById("next")
-const prev=document.getElementById("prev")
+const next = document.getElementById("next")
+const prev = document.getElementById("prev")
+
+const durtn = document.getElementById("duration")
+const crntTime = document.getElementById("current-time")
+const prgrs = document.querySelector(".progress")
+const prgrsClick = document.querySelector(".prog-div")
+
 
 const song = [
-  
+
     {
-        names:"songOne",
+        names: "songOne",
         title: "Khas",
         artist: "Dino James"
     },
     {
-        names:"songTwo",
+        names: "songTwo",
         title: "Let's crack it",
         artist: "Naezy"
     },
     {
-        names:"songThree",
+        names: "songThree",
         title: "Unstoppable",
         artist: "Dino James"
 
@@ -29,7 +35,7 @@ const song = [
 let playing = false
 // function to play
 // as initially we gave for playing false it will play on click
-const playMusic=()=> {
+const playMusic = () => {
     // making playing true so on other click we can go to pause function
     playing = true
     playSound.play()
@@ -38,62 +44,111 @@ const playMusic=()=> {
     image.classList.add("animation")
     //change icon from play to pause using class names
     music.classList.replace("fa-play", "fa-pause")
+    loops=false
 
 }
 
 // function to pause
-const pauseMusic=()=> {
+const pauseMusic = () => {
     // we make playing false so on next click play function can be called 
     playing = false
     playSound.pause()
-    
+
     image.classList.remove("animation")
     music.classList.replace("fa-pause", "fa-play")
 }
 
 
 music.addEventListener("click", () => {
-// if playing is true call pauseMusic function else call playMusic if false
-    playing?pauseMusic():playMusic();
-    
+    // if playing is true call pauseMusic function else call playMusic if false
+    playing ? pauseMusic() : playMusic();
+
 })
 
 // to change song 
-const loadSong=(song)=> {
- 
-    title.textContent=song.title;
-    artist.textContent=song.artist;
-    playSound.src="music/"+song.names+".mp3"
-    image.src="images/"+song.names+".jpg"
+const loadSong = (song) => {
 
-    
+    title.textContent = song.title;
+    artist.textContent = song.artist;
+    playSound.src = "music/" + song.names + ".mp3"
+    image.src = "images/" + song.names + ".jpg"
+
+
 }
 // for count
-let songsIndex=0
+let songsIndex = 0
 // functin for next song
-const nextSong =()=>{
-    songsIndex=(songsIndex+1)%song.length
+const nextSong = () => {
+    songsIndex = (songsIndex + 1) % song.length
     loadSong(song[songsIndex])
-    if(playing){
+    if (playing) {
         playMusic()
-    }else{
+    } else {
         pauseMusic()
     }
-    
+
 
 }
 // function for previous song
-const prevSong=()=>{
-  
-   songsIndex=(songsIndex-1+song.length)%song.length;
+const prevSong = () => {
+
+    songsIndex = (songsIndex - 1 + song.length) % song.length;
     loadSong(song[songsIndex])
-    if(playing){
+    if (playing) {
         playMusic()
-    }else{
+    } else {
         pauseMusic()
     }
 
 }
 
-next.addEventListener("click",nextSong)
-prev.addEventListener("click",prevSong)
+// progress bar
+playSound.addEventListener("timeupdate", (e) => {
+    // const {currentTime,duration}=e.target or below is seperately defining
+    const currentTime = Math.floor(e.target.currentTime)
+
+    const duration = e.target.duration;
+    const progressShow = currentTime / duration * 100
+    prgrs.style.width = `${progressShow}%`;
+    const min = Math.floor(duration / 60);
+    const sec = Math.floor(duration % 60)
+
+    if (duration) {
+
+        durtn.textContent = `${min}:${sec}`;
+    }
+
+    //    current time update
+
+    let minCur = Math.floor(currentTime / 60);
+    let secCur = Math.floor(currentTime % 60)
+    if (secCur < 10) {
+        secCur = `0${secCur}`
+    }
+    if (duration) {
+        crntTime.textContent = `${minCur}:${secCur}`;
+    }
+})
+
+prgrsClick.addEventListener("click", (e) => {
+    const {duration} = playSound
+
+    let curClickWidth = e.offsetX
+    let totalWidth
+    if (e.target.parentElement.clientWidth <= 500) {
+        totalWidth = e.target.parentElement.clientWidth
+    } else if (e.target.parentElement.clientWidth > 500) {
+        totalWidth = e.target.clientWidth
+    }
+
+    let currentWidth = curClickWidth / totalWidth * duration
+
+    playSound.currentTime = currentWidth
+
+})
+
+playSound.addEventListener("ended", nextSong)
+next.addEventListener("click", nextSong)
+prev.addEventListener("click", prevSong)
+
+
